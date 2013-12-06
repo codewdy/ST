ac3808dfdb2131b89035c9a71eb0943e4cf649ee
codewdy <codewdy@gmail.com>
2013-12-06 08:36:52 +0800

    写了两个版本的sg_op和db_op以利用编译器优化

--- src/frontend/lexUtils.h
+++ src/frontend/lexUtils.h
@@ -2,4 +2,6 @@
 #define ST_FRONTEND_LEXUTILS
 #define SG_OP(x) (#x[0])
 #define DB_OP(x) (#x[0] * 256 + #x[1])
+inline int sg_op(char* x) {return x[0];}
+inline int db_op(char* x) {return x[0] * 256 + x[1];}
 #endif
